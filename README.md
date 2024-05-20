1c8b7f8192a2fe24a8a2ab5a9c3889810ab2db54
Orçun Çolak <orcun.colak@hazelcast.com>
2024-05-20 11:08:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Read Me First
+
+The following original idea is from  
+https://webcache.googleusercontent.com/search?q=cache:https://medium.com/@thomasjay200/spring-boot-json-logs-in-5-minutes-0f9b4c4ccd4d
+
---
+++ src/main/java/com/colak/springlogbacktutorial/SpringLogbackTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.colak.springlogbacktutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogbackTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogbackTutorialApplication.class, args);
+	}
+
+}
