a7e31b7b87e885ac857e6ac0d13e77038e1d6aef
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-26 09:45:32 +0300

    upgrade spring

--- src/main/java/com/colak/springtutorial/controller/HelloController.java
+++ src/main/java/com/colak/springtutorial/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.colak.springlogbacktutorial.controller;
+package com.colak.springtutorial.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
